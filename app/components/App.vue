b83025ac282c69b3e521c9ccaea894bd6fd139e0
John Cotton <john@1080people.com>
2018-12-04 11:49:22 -0500

    remove comments from App.vue

--- app/components/App.vue
+++ app/components/App.vue
@@ -55,57 +55,6 @@ import * as color from 'tns-core-modules/color'
 import clock from '~/assets/scripts/clock'
 import timecodeTimer from '~/assets/scripts/timecodeTimer'
 
-// let fr, hours, minutes, seconds, frames
-// hours = 0
-// minutes = 0
-// seconds = 0
-// frames = 0      
-// function counter(framerate){
-//     let frameString, secondString, minuteString, hourString
-//     if (frames < framerate ){
-        
-//     frames++
-        
-//     } else if (frames === framerate){
-//         seconds ++
-//         frames = 0
-//     }
-//     if (seconds === 60) {
-//         seconds = 0
-//         minutes ++
-//     }
-//     if (minutes === 60){
-//         minutes = 0
-//         hours ++
-//     }
-//     if (hours === 13){
-//         hours = 1
-//     }
-
-//     if (parseInt(frames, 10) < 10) {
-//                 frameString = `0${frames}`
-//         } else {
-//             frameString = frames
-//         }
-//     if (parseInt(seconds, 10) < 10) {
-//             secondString = `0${seconds}`
-//         } else {
-//             secondString = seconds
-//         }
-//             if (parseInt(minutes, 10) < 10) {
-//                 minuteString = `0${minutes}`
-//             } else {
-//                  minuteString = minutes
-//             }
-//             if (parseInt(hours, 10) < 10) {
-//                 hourString = `0${hours}`
-//             } else {
-//                  hourString = hours
-//             }
-    
-//     return `${hourString}:${minuteString}:${secondString}:${frameString}`
-
-// }
   export default {
     data() {
       return {
